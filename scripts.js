6e4a7ebe028fcad5a6109e1e15e74feaee5080a0
Jason Kartz <jasonkartz@gmail.com>
2022-08-13 11:47:59 -0500

    mobile img carousel

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+//Menu
+
 const nav = document.getElementById("nav");
 const menuBtn = document.getElementById("btn-menu");
 const cartAvatar = document.getElementById("cart-avatar-containter");
@@ -30,3 +32,37 @@ function menuClose() {
 
 menuBtn.addEventListener("click", menuToggle);
 main.addEventListener("click", menuClose);
+
+//Image carousel
+
+const imgBtnPrevious = document.getElementById("img-btn-previous");
+const imgBtnNext = document.getElementById("img-btn-next");
+const imgCarousel = document.getElementById("img-carousel");
+
+const images = [1, 2, 3, 4];
+let currentImg = 0;
+
+function imgChange() {
+  imgCarousel.style.background = `url("./images/image-product-${images[currentImg]}.jpg") no-repeat`;
+  imgCarousel.style.backgroundSize = "cover";
+}
+
+function prevImg() {
+  if (currentImg === 0) {
+    currentImg = images.length - 1;
+  } else {
+    currentImg--;
+  }
+  imgChange();
+}
+function nextImg() {
+  if (currentImg === images.length - 1) {
+    currentImg = 0;
+  } else {
+    currentImg++;
+  }
+  imgChange();
+}
+
+imgBtnPrevious.addEventListener("click", prevImg);
+imgBtnNext.addEventListener("click", nextImg);

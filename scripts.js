86500cd121ed39bda76ce6afaa979b2570f28a57
Jason Kartz <jasonkartz@gmail.com>
2022-08-18 21:12:22 -0500

    working on cart functionality

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-//Menu
+// ***** Menu *****
 
 const nav = document.getElementById("nav");
 const menuBtn = document.getElementById("btn-menu");
@@ -33,7 +33,7 @@ function menuClose() {
 menuBtn.addEventListener("click", menuToggle);
 main.addEventListener("click", menuClose);
 
-//Image carousel
+// ***** Image carousel *****
 
 const imgBtnPrevious = document.getElementById("img-btn-previous");
 const imgBtnNext = document.getElementById("img-btn-next");
@@ -66,3 +66,38 @@ function nextImg() {
 
 imgBtnPrevious.addEventListener("click", prevImg);
 imgBtnNext.addEventListener("click", nextImg);
+
+// ***** Cart *****
+
+const qtyAmount = document.getElementById("qty-amount");
+const qtyMinusBtn = document.getElementById("qty-minus");
+const qtyPlusBtn = document.getElementById("qty-plus");
+const addToCartBtn = document.getElementById("add-to-cart-btn");
+
+let qty = 0;
+let cartQty = 0;
+
+function increaseQty() {
+  qty++;
+  qtyAmount.innerText = qty;
+}
+
+function decreaseQty() {
+  if (qty === 0) {
+    qtyAmount.innerText = "0";
+  } else {
+    qty--;
+    qtyAmount.innerText = qty;
+  }
+}
+
+function addToCart() {
+  cartQty = qty;
+  qty = 0;
+  qtyAmount.innerText = qty;
+  console.log(cartQty);
+}
+
+qtyPlusBtn.addEventListener("click", increaseQty);
+qtyMinusBtn.addEventListener("click", decreaseQty);
+addToCartBtn.addEventListener("click", addToCart);

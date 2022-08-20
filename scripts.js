19167dab79d5d32a275d5b987c68ba1a204c1568
Jason Kartz <jasonkartz@gmail.com>
2022-08-20 16:48:56 -0500

    mobile cart functionality finished

--- scripts.js
+++ scripts.js
@@ -1,38 +1,3 @@
-// ***** Menu *****
-
-const nav = document.getElementById("nav");
-const menuBtn = document.getElementById("btn-menu");
-const cartAvatar = document.getElementById("cart-avatar-containter");
-const main = document.getElementById("main");
-
-let isMenuOpen = false;
-
-function menuToggle() {
-  if (!isMenuOpen) {
-    nav.style.display = "block";
-    menuBtn.style.background =
-      "url('./images/icon-menu.svg') no-repeat, url('./images/icon-close.svg') no-repeat";
-    cartAvatar.style.display = "none";
-  } else {
-    nav.style.display = "none";
-    menuBtn.style.background = "url('./images/icon-menu.svg') no-repeat";
-    cartAvatar.style.display = "flex";
-  }
-  isMenuOpen = !isMenuOpen;
-}
-
-function menuClose() {
-  if (isMenuOpen) {
-    nav.style.display = "none";
-    menuBtn.style.background = "url('./images/icon-menu.svg') no-repeat";
-    cartAvatar.style.display = "flex";
-    isMenuOpen = !isMenuOpen;
-  }
-}
-
-menuBtn.addEventListener("click", menuToggle);
-main.addEventListener("click", menuClose);
-
 // ***** Image carousel *****
 
 const imgBtnPrevious = document.getElementById("img-btn-previous");
@@ -69,13 +34,34 @@ imgBtnNext.addEventListener("click", nextImg);
 
 // ***** Cart *****
 
+const cartIcon = document.getElementById("cart-icon");
+const cartQtyPreview = document.getElementById("cart-qty-preview");
+const cartEmpty = document.getElementById("cart-empty");
+const cartProduct = document.getElementById("cart-product");
+const cartQty = document.getElementById("cart-qty");
+const cartTotal = document.getElementById("cart-total");
+const cartDeleteItem = document.getElementById("cart-delete-item");
+const btnCheckout = document.getElementById("btn-checkout");
+const cart = document.getElementById("cart");
+
 const qtyAmount = document.getElementById("qty-amount");
 const qtyMinusBtn = document.getElementById("qty-minus");
 const qtyPlusBtn = document.getElementById("qty-plus");
 const addToCartBtn = document.getElementById("add-to-cart-btn");
 
+let showCart = false;
 let qty = 0;
-let cartQty = 0;
+let addToCartQty = 0;
+
+function toggleCart() {
+  if (showCart === false) {
+    cart.style.visibility = "visible";
+    showCart = true;
+  } else {
+    cart.style.visibility = "hidden";
+    showCart = false;
+  }
+}
 
 function increaseQty() {
   qty++;
@@ -91,13 +77,86 @@ function decreaseQty() {
   }
 }
 
-function addToCart() {
-  cartQty = qty;
+function tallyCart() {
   qty = 0;
   qtyAmount.innerText = qty;
-  console.log(cartQty);
+  cartQtyPreview.innerText = addToCartQty;
+  cartQty.innerText = addToCartQty;
+  cartTotal.innerText = `$${addToCartQty * 125}.00`;
 }
 
+function addToCart() {
+  if (qty > 0 && addToCartQty === 0) {
+    addToCartQty = qty;
+    tallyCart();
+    cartQtyPreview.style.visibility = "visible";
+    cartEmpty.style.display = "none";
+    cartProduct.style.display = "flex";
+    btnCheckout.style.display = "block";
+  } else if (qty > 0 && addToCartQty > 0) {
+    addToCartQty += qty;
+    tallyCart();
+  }
+}
+
+function emptyCart() {
+  if (addToCartQty > 0) {
+    cartQtyPreview.style.visibility = "hidden";
+    cartEmpty.style.display = "block";
+    cartProduct.style.display = "none";
+    btnCheckout.style.display = "none";
+    addToCartQty = 0;
+    qtyAmount.innerText = 0;
+    cartQtyPreview.innerText = 0;
+    cartQty.innerText = 0;
+    cartTotal.innerText = `$${0}.00`;
+  }
+}
+function checkout() {
+  emptyCart();
+  toggleCart();
+}
+cartIcon.addEventListener("click", toggleCart);
 qtyPlusBtn.addEventListener("click", increaseQty);
 qtyMinusBtn.addEventListener("click", decreaseQty);
 addToCartBtn.addEventListener("click", addToCart);
+cartDeleteItem.addEventListener("click", emptyCart);
+btnCheckout.addEventListener("click", checkout);
+
+// ***** Menu *****
+
+const nav = document.getElementById("nav");
+const menuBtn = document.getElementById("btn-menu");
+const cartAvatar = document.getElementById("cart-avatar-containter");
+const main = document.getElementById("main");
+
+let isMenuOpen = false;
+
+function menuToggle() {
+  if (!isMenuOpen) {
+    nav.style.display = "block";
+    menuBtn.style.background =
+      "url('./images/icon-menu.svg') no-repeat, url('./images/icon-close.svg') no-repeat";
+    cartAvatar.style.display = "none";
+    //closes cart window
+    cart.style.visibility = "hidden";
+    showCart = false;
+  } else {
+    nav.style.display = "none";
+    menuBtn.style.background = "url('./images/icon-menu.svg') no-repeat";
+    cartAvatar.style.display = "flex";
+  }
+  isMenuOpen = !isMenuOpen;
+}
+
+function menuClose() {
+  if (isMenuOpen) {
+    nav.style.display = "none";
+    menuBtn.style.background = "url('./images/icon-menu.svg') no-repeat";
+    cartAvatar.style.display = "flex";
+    isMenuOpen = !isMenuOpen;
+  }
+}
+
+menuBtn.addEventListener("click", menuToggle);
+main.addEventListener("click", menuClose);
